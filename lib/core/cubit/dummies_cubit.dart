54769bb41a83441245407bed107ec608e7dd9bbe
oneeall <danniel.alvianto@gmail.com>
2026-08-17 11:41:44 +0700

    chore(core) : support dart-define for dummies mode in dummies cubit

--- lib/core/cubit/dummies_cubit.dart
+++ lib/core/cubit/dummies_cubit.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DummiesCubit extends Cubit<bool> {
-  DummiesCubit() : super(false);
+  // Use a Dart define to set the default state.
+  // Usage: flutter run --dart-define=USE_DUMMIES=true
+  static const bool _defaultMode = bool.fromEnvironment('USE_DUMMIES', defaultValue: false);
+
+  DummiesCubit() : super(_defaultMode);
 
   void toggle() => emit(!state);
 }
